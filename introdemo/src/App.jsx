ab10e73006ef1483590b7b5ebbab5319afe70211
reignu <ungier.adam@gmail.com>
2025-06-01 17:27:26 +0100

    Part 1c - page re-rendering

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,22 +1,7 @@
-const Hello = (props) => {
-  console.log(props)
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='George' age={26+10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
--- introdemo/src/main.jsx
+++ introdemo/src/main.jsx
@@ -2,4 +2,19 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const refresh = () => {
+  root.render(
+    <App counter={counter}/>
+  )
+}
+
+// setInterval (() => {
+//   refresh()
+//   counter += 1
+// }, 1000)
+
+
