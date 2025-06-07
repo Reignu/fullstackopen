2120fb30b32e1ba8b693912094cc2834a5136d1f
reignu <ungier.adam@gmail.com>
2025-06-07 18:40:47 +0100

    Refactor to pass state to child components

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -3,13 +3,28 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
+  const incrementBy1 = () => setCounter(counter + 1)
+  const setto0 = () => setCounter(0)
+
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>Add</button>
-      <button onClick={() => setCounter(0)}>Reset</button>
+      <Display counter={counter} />
+      <Button onClick={incrementBy1} text='Add' />
+      <Button onClick={setto0} text='Reset' />
     </div>
   )
 }
 
 export default App
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
