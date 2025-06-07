5b03c281b7dff8d65ec7bbd40395c4534d1333bd
reignu <ungier.adam@gmail.com>
2025-06-07 19:28:58 +0100

    Added log statements to visualise dataflow

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with value ', counter)
 
-  const incrementBy1 = () => setCounter(counter + 1)
-  const setto0 = () => setCounter(0)
+  const incrementBy1 = () => {
+    setCounter(counter + 1)
+    console.log('rendering with value before increment ', counter)
+  }
+  const decrementBy1 = () => {
+    setCounter(counter - 1)
+    console.log('rendering with value before decrement ', counter)
+  }
+  const setto0 = () => {
+    console.log('rendering with value before reset ', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
       <Display counter={counter} />
-      <Button onClick={incrementBy1} text='Add' />
+      <Button onClick={incrementBy1} text='+' />
+      <Button onClick={decrementBy1} text='-' />
       <Button onClick={setto0} text='Reset' />
     </div>
   )
